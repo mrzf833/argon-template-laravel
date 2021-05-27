e002627e1cfd6ee1532e46fe0d52d54bc39ebbb7
mrzf833 <palaktais@gmail.com>
2021-05-27 20:55:15 +0700

    add layout1

---
+++ resources/views/layouts/layout1/layout.blade.php
@@ -0,0 +1,62 @@
+<!--
+
+=========================================================
+* Argon Dashboard - v1.1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
+    </title>
+    <!-- Favicon -->
+    <link href="{{ asset('assets/img/brand/favicon.png') }}" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link href="{{ asset('assets/js/plugins/nucleo/css/nucleo.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="{{ asset('assets/css/argon-dashboard.css?v=1.1.1') }}" rel="stylesheet" />
+</head>
+
+<body class="bg-default">
+    <div class="main-content">
+        <!-- Navbar -->
+        @include('layouts.layout1.navbar')
+        <!-- Header -->
+        @include('layouts.layout1.header')
+        <!-- Page content -->
+        @yield('content')
+        @include('layouts.layout1.footer')
+    </div>
+    <!--   Core   -->
+    <script src="{{ asset('assets/js/plugins/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!--   Optional JS   -->
+    <!--   Argon JS   -->
+    <script src="{{ asset('assets/js/argon-dashboard.min.js?v=1.1.1') }}"></script>
+    <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+    <script>
+    window.TrackJS &&
+    TrackJS.install({
+        token: "ee6fab19c5a04ac1a32a645abde4613a",
+        application: "argon-dashboard-free"
+    });
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ------------------- route default template argon ----------------
+Route::get('login', function(){
+    return view('login');
+})->name('login');
+
+Route::get('dashboard', function(){
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('register', function(){
+    return view('register');
+})->name('register');
+// -----------------------------------------------------------------
